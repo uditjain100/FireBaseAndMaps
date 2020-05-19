56bf49b7d23ea72a5ed7a40fb5095ea7fa3b40e8
Udit Jain <jain30udit@gmail.com>
2020-05-20 03:01:26 +0530

    CameraX Rotation feature Added

--- app/src/main/java/udit/programmer/co/firebase/CameraActivity.kt
+++ app/src/main/java/udit/programmer/co/firebase/CameraActivity.kt
@@ -1,9 +1,11 @@
 package udit.programmer.co.firebase
 
 import android.content.pm.PackageManager
+import android.opengl.Matrix
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Rational
+import android.view.Surface
 import android.view.ViewGroup
 import androidx.camera.core.CameraX
 import androidx.camera.core.Preview
@@ -27,23 +29,43 @@ class CameraActivity : AppCompatActivity() {
                 setCamera()
             }
         } else {
-            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA),1234)
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.CAMERA),
+                1234
+            )
         }
 
     }
 
     private fun setCamera() {
-
         val previewConfig = PreviewConfig.Builder().apply {
             setTargetAspectRatio(Rational.POSITIVE_INFINITY)
+            setLensFacing(CameraX.LensFacing.BACK)
         }.build()
         val preview = Preview(previewConfig)
         preview.setOnPreviewOutputUpdateListener {
             val parent = texture_layout.parent as ViewGroup
             parent.removeView(texture_layout)
             parent.addView(texture_layout, 0)
+            updateCamera()
             texture_layout.surfaceTexture = it.surfaceTexture
         }
         CameraX.bindToLifecycle(this, preview)
+    }
+
+    private fun updateCamera() {
+        val matrix = android.graphics.Matrix()
+        val centerX = texture_layout.width / 2f
+        val centerY = texture_layout.height / 2f
+        val rotationDegree = when (texture_layout.display.rotation) {
+            Surface.ROTATION_0 -> 0
+            Surface.ROTATION_90 -> 90
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_270 -> 270
+            else -> return
+        }
+        matrix.postRotate(rotationDegree.toFloat(), centerX, centerY)
+        texture_layout.setTransform(matrix)
     }
 }
