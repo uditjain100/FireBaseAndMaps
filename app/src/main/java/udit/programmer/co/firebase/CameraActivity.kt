ea3f8e71dd7493f2032c70dc80646ced1b7c7762
Udit Jain <jain30udit@gmail.com>
2020-05-20 14:59:25 +0530

    CameraX Capturing feature Added

--- app/src/main/java/udit/programmer/co/firebase/CameraActivity.kt
+++ app/src/main/java/udit/programmer/co/firebase/CameraActivity.kt
@@ -4,14 +4,14 @@ import android.content.pm.PackageManager
 import android.opengl.Matrix
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.util.Rational
 import android.view.Surface
 import android.view.ViewGroup
-import androidx.camera.core.CameraX
-import androidx.camera.core.Preview
-import androidx.camera.core.PreviewConfig
+import androidx.camera.core.*
 import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_camera.*
+import java.io.File
 import java.util.jar.Manifest
 
 class CameraActivity : AppCompatActivity() {
@@ -39,6 +39,32 @@ class CameraActivity : AppCompatActivity() {
     }
 
     private fun setCamera() {
+
+        val imageCaptureConfig = ImageCaptureConfig.Builder().apply {
+            setTargetAspectRatio(Rational.POSITIVE_INFINITY)
+            setCaptureMode(ImageCapture.CaptureMode.MAX_QUALITY)
+        }.build()
+
+        val imageCapture = ImageCapture(imageCaptureConfig)
+
+        capture_image.setOnClickListener {
+            val file = File(externalMediaDirs.first(), "${System.currentTimeMillis()}.jpg")
+            imageCapture.takePicture(file, object : ImageCapture.OnImageSavedListener {
+                override fun onImageSaved(file: File) {
+                    Log.i("IMAGE CAPTURED", "Image is Captured")
+                }
+
+                override fun onError(
+                    useCaseError: ImageCapture.UseCaseError,
+                    message: String,
+                    cause: Throwable?
+                ) {
+                    Log.i("IMAGE CAPTURED", "Error in Capturing")
+                }
+
+            })
+        }
+
         val previewConfig = PreviewConfig.Builder().apply {
             setTargetAspectRatio(Rational.POSITIVE_INFINITY)
             setLensFacing(CameraX.LensFacing.BACK)
@@ -51,7 +77,7 @@ class CameraActivity : AppCompatActivity() {
             updateCamera()
             texture_layout.surfaceTexture = it.surfaceTexture
         }
-        CameraX.bindToLifecycle(this, preview)
+        CameraX.bindToLifecycle(this, preview, imageCapture)
     }
 
     private fun updateCamera() {
