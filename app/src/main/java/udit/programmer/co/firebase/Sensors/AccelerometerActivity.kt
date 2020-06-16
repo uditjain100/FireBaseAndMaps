74a50e4043eb3acb8c832c19b7be1fe329514a93
Udit Jain <jain30udit@gmail.com>
2020-06-17 00:23:52 +0530

    Shuffling Work Added

--- app/src/main/java/udit/programmer/co/firebase/Sensors/AccelerometerActivity.kt
+++ app/src/main/java/udit/programmer/co/firebase/Sensors/AccelerometerActivity.kt
@@ -2,12 +2,14 @@ package udit.programmer.co.firebase.Sensors
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Color
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_accelerometer.*
 import udit.programmer.co.firebase.R
 
@@ -15,6 +17,7 @@ class AccelerometerActivity : AppCompatActivity(), SensorEventListener {
 
     lateinit var sensorManager: SensorManager
     lateinit var accelerometor: Sensor
+    var temp = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,8 +44,25 @@ class AccelerometerActivity : AppCompatActivity(), SensorEventListener {
 
     @SuppressLint("SetTextI18n")
     override fun onSensorChanged(event: SensorEvent?) {
-        x_tv.text = "X : " + event!!.values[0].toString()
-        y_tv.text = "Y : " + event!!.values[1].toString()
-        z_tv.text = "Z : " + event!!.values[2].toString()
+        val x = event!!.values[0]
+        val y = event.values[1]
+        val z = event.values[2]
+
+        x_tv.text = "X : $x"
+        y_tv.text = "Y : $y"
+        z_tv.text = "Z : $z"
+
+        sufflingWork(x, y, z)
+    }
+
+    private fun sufflingWork(x: Float, y: Float, z: Float) {
+        val sqrt =
+            (x * x + y * y + z * z) / (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH)
+        if (sqrt >= 3) {
+            Toast.makeText(this, "Device Shuffled", Toast.LENGTH_LONG).show()
+            if (temp) accelerometer_layout.setBackgroundColor(Color.RED)
+            else accelerometer_layout.setBackgroundColor(Color.CYAN)
+        }
+        temp = !temp
     }
 }
