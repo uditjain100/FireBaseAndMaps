c13a6c120a6746c28eda23f04b8ab947f808043e
Udit Jain <jain30udit@gmail.com>
2020-06-22 20:33:17 +0530

    Searchable Spinner with Firebase Work Added

--- app/src/main/java/udit/programmer/co/firebase/FireBase/SpinnerActivity.kt
+++ app/src/main/java/udit/programmer/co/firebase/FireBase/SpinnerActivity.kt
@@ -3,61 +3,62 @@ package udit.programmer.co.firebase.FireBase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_spinner.*
 import udit.programmer.co.firebase.R
 
-class SpinnerActivity : AppCompatActivity()
-//    , FirebaseLoadDone
-{
+class SpinnerActivity : AppCompatActivity() {
 
-//    lateinit var quizRef: DatabaseReference
-//    lateinit var firebaseLoadDone: FirebaseLoadDone
+    private lateinit var databaseReference: DatabaseReference
+    private var dataList: MutableList<String> = mutableListOf()
+    private lateinit var adapter: ArrayAdapter<String>
+    private lateinit var listener: ValueEventListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_spinner)
 
-        val list = listOf<String>("One", "Two", "Three")
-        val adapter = ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, list)
+//        val list = listOf<String>("One", "Two", "Three")
+//        val adapter = ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, list)
+//        searchable_spinner.adapter = adapter
+
+        databaseReference = FirebaseDatabase.getInstance().reference
+        adapter = ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, dataList)
         searchable_spinner.adapter = adapter
 
-//        firebaseLoadDone = this
-//        quizRef = FirebaseDatabase.getInstance().reference
-//        quizRef.addValueEventListener(object : ValueEventListener {
-//            var EDMTQuizList: MutableList<EDMTQuiz> = ArrayList()
-//            override fun onCancelled(p0: DatabaseError) {
-//                firebaseLoadDone.onFirebaseLoadFailed(p0.message)
-//            }
-//
-//            override fun onDataChange(p0: DataSnapshot) {
-//                for (quizSnapshot in p0.children) {
-//                    EDMTQuizList.add(quizSnapshot.getValue<EDMTQuiz>(EDMTQuiz::class.java)!!)
-//                }
-//                firebaseLoadDone.onFirebaseLoadSuccess(EDMTQuizList)
-//            }
-//        })
-
+        btn_click.setOnClickListener {
+            additionWork()
+        }
+        retrieveData()
     }
 
-//    override fun onFirebaseLoadSuccess(EDMTQuizList: List<EDMTQuiz>) {
-//        val question = getQuestion(EDMTQuizList)
-//        val adapter = ArrayAdapter<String>(
-//            this,
-//            android.R.layout.simple_expandable_list_item_1,
-//            question
-//        )
-//        searchable_spinner.adapter = adapter
-//    }
-//
-//    private fun getQuestion(EDMTQuizList: List<EDMTQuiz>): List<String> {
-//        val list = ArrayList<String>()
-//        for (movie in EDMTQuizList)
-//            list.add(movie.questionText!!)
-//        return list
-//    }
-//
-//    override fun onFirebaseLoadFailed(message: String?) {
-//    }
+    private fun additionWork() {
+        var data = tvv.text.toString().trim()
+        databaseReference.push().setValue(data).addOnCompleteListener {
+            tvv.setText("")
+            dataList.clear()
+            retrieveData()
+            adapter.notifyDataSetChanged()
+            Toast.makeText(this, "Data added Successfully", Toast.LENGTH_LONG).show()
+        }.addOnFailureListener {
+            Toast.makeText(this, "Data addition Failed", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun retrieveData() {
+        listener = databaseReference.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                for (dataSnapshot in p0.children)
+                    dataList.add(dataSnapshot.value.toString())
+                adapter.notifyDataSetChanged()
+            }
+        })
+    }
+
 }
