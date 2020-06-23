afb088ff0f056d807a81623c2c7453c19a4c0516
Udit Jain <jain30udit@gmail.com>
2020-06-23 15:02:42 +0530

    View Binding Work Added

--- app/src/main/java/udit/programmer/co/firebase/JetPack/DataBindingActivity.kt
+++ app/src/main/java/udit/programmer/co/firebase/JetPack/DataBindingActivity.kt
@@ -7,12 +7,20 @@ import android.text.TextWatcher
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_data_binding.*
 import udit.programmer.co.firebase.R
+import udit.programmer.co.firebase.databinding.ActivityMainBinding
 
 class DataBindingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_data_binding)
 
+//        View Binding Work is more safer in case of Null Pointer Exceptions
+//        Such as in case of changing Orientations , it can handle it easily
+
+//        Below 2 lines code is used to add view binding features in this activity
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        super.onCreate(savedInstanceState)
         tvv.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
 
@@ -24,9 +32,9 @@ class DataBindingActivity : AppCompatActivity() {
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 Toast.makeText(this@DataBindingActivity, "$s", Toast.LENGTH_SHORT).show()
+                text_view.setText(s.toString())
             }
         })
-
 
     }
 }
